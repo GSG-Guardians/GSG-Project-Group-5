5cc676132f5da844aa81504630693f168f5bd6d0
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2026-01-28 21:21:12 +0200

    (feat):Generate a swagger wrapper for unified response: generic wrapper  and you only pass the type of data in the response

---
+++ src/helpers/baseSwaggerDTO.helpers.ts
@@ -0,0 +1,33 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class MetaPaginationSwaggerDto {
+  @ApiProperty({ example: 1 }) page: number;
+  @ApiProperty({ example: 10 }) limit: number;
+  @ApiProperty({ example: 5 }) totalPages: number;
+  @ApiProperty({ example: 47 }) totalItems: number;
+}
+
+export class SuccessOneSwaggerDto<T> {
+  @ApiProperty({ example: true })
+  success: true;
+
+  // data type injected by helper (allOf)
+  data: T;
+
+  @ApiPropertyOptional({ example: 'Operation successful' })
+  message?: string;
+}
+
+export class SuccessPaginatedSwaggerDto<T> {
+  @ApiProperty({ example: true })
+  success: true;
+
+  // data type injected by helper (allOf)
+  data: T[];
+
+  @ApiProperty({ type: MetaPaginationSwaggerDto })
+  meta: MetaPaginationSwaggerDto;
+
+  @ApiPropertyOptional({ example: 'Operation successful' })
+  message?: string;
+}
---
+++ src/helpers/swaggerDTOWrapper.helpers.ts
@@ -0,0 +1,45 @@
+import { applyDecorators, Type } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+import {
+  MetaPaginationSwaggerDto,
+  SuccessOneSwaggerDto,
+  SuccessPaginatedSwaggerDto,
+} from './baseSwaggerDTO.helpers';
+
+export const ApiSuccess = <TModel extends Type<any>>(model: TModel) =>
+  applyDecorators(
+    ApiExtraModels(SuccessOneSwaggerDto, model),
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(SuccessOneSwaggerDto) },
+          {
+            properties: {
+              data: { $ref: getSchemaPath(model) },
+            },
+          },
+        ],
+      },
+    }),
+  );
+
+export const ApiSuccessPaginated = <TModel extends Type<any>>(model: TModel) =>
+  applyDecorators(
+    ApiExtraModels(SuccessPaginatedSwaggerDto, MetaPaginationSwaggerDto, model),
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(SuccessPaginatedSwaggerDto) },
+          {
+            properties: {
+              data: {
+                type: 'array',
+                items: { $ref: getSchemaPath(model) },
+              },
+              meta: { $ref: getSchemaPath(MetaPaginationSwaggerDto) },
+            },
+          },
+        ],
+      },
+    }),
+  );
