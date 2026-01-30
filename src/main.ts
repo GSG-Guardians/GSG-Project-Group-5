8818cea13c9ec2eaec6f96f4d579f97365f0a53c
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2026-01-30 22:15:53 +0200

    feat: add Swagger UI for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Finance Assistance API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    // If you use Bearer JWT:
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    useGlobalPrefix: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
