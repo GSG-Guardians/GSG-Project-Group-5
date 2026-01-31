40b2bdc3b413327a01dd1a1d1c308d0c679f4d89
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2026-01-31 23:42:49 +0200

    feat: Update Vercel configuration for API routing and streamline TypeORM entity discovery by removing the `index.ts` aggregation file.

--- database/data-source.ts
+++ database/data-source.ts
@@ -2,14 +2,13 @@ import 'dotenv/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
 import { join } from 'path';
-import * as entities from './entities';
 
 config({ path: '.env' });
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  entities: Object.values(entities),
+  entities: [join(__dirname, 'entities', '*.{js,ts}')],
   migrations: [join(__dirname, 'migrations', '*.{js,ts}')],
   synchronize: false,
 };
