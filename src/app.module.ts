1be1331614d709f52592d566b6627b15ed23cad3
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2026-02-04 15:58:53 +0200

    feat: implement global JWT authentication guard with public route decorator and authentication response DTO.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from '../database/data-source';
 import { BillsModule } from './modules/bills/bills.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { UnifiedResponseInterceptor } from './interceptors/unifiedResponse.interceptor';
 import { UserModule } from './modules/user/user.module';
 import { DatabaseModule } from './modules/database/database.module';
@@ -16,6 +16,7 @@ import { FinancialReportModule } from './modules/financial-report/financial-repo
 import { CurrencyModule } from './modules/currency/currency.module';
 import { ExpensesModule } from './modules/expenses/expenses.module';
 import { MailModule } from './modules/mail/mail.module';
+import { AuthGuard } from './modules/auth/guards/auth.guard';
 
 @Module({
   imports: [
@@ -41,6 +42,10 @@ import { MailModule } from './modules/mail/mail.module';
     {
       provide: APP_INTERCEPTOR,
       useClass: UnifiedResponseInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
     },
   ],
 })
