e9bdf6f266fee7556bc43709222c76f7fcc5694e
Moamen Al-Yazouri <moaamenalyazouri@gmail.com>
2026-02-19 14:28:37 +0200

    fix

--- src/modules/ai/ai.module.ts
+++ src/modules/ai/ai.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AiService } from './ai.service';
-import { AiController } from './ai.controller';
 import { Chat } from '../../../database/entities/chat.entities';
 import { Message } from '../../../database/entities/message.entities';
 import { BillsModule } from '../bills/bills.module';
@@ -10,6 +8,8 @@ import { BudgetModule } from '../budget/budget.module';
 import { DebtModule } from '../debt/debt.module';
 import { IncomeModule } from '../income/income.module';
 import { ChatModule } from '../chat/chat.module';
+import { AiController } from './ai.controller';
+import { AiService } from './ai.service';
 
 @Module({
   imports: [
